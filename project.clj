a53270b4e2be1a7ba5d231d76476b8d2c76d76d3
oysteinjakobsen <ojapublic@manglerud.net>
2014-09-26 21:20:21 +0200

    Upgraded to Clojure 1.6 and to a Neocons snapshot build with a bug fix to enable GrapheneDB support.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/oysteinjakobsen/minesweeper-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.7.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
-                 [clojurewerkz/neocons "3.0.0"]
+                 [clojurewerkz/neocons "3.1.0-SNAPSHOT"]
                  [org.clojure/tools.cli "0.3.1"]]
   :profiles {:dev {:dependencies [[speclj "3.0.1"]]}}
   :plugins [[speclj "3.0.1"]]
